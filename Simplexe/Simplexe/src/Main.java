e9fabafcb9e61e9d7712217093bc4bd29749b22e
nico.tomio <nico.tomio@f8d6e94b-3401-3f3e-5a1a-e53be2de7118>
2011-05-16 20:00:13 +0000

    Mise en place du SVN

---
+++ Simplexe/Simplexe/src/Main.java
@@ -0,0 +1,14 @@
+import Interface.InterfaceAccueil;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfaceAccueil accueil = new InterfaceAccueil();
+	}
+
+}
