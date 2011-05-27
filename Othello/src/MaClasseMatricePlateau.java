abbbc6b7de80d868bfc0a1d36aae3dad56433694
andre.miras <andre.miras@f8d6e94b-3401-3f3e-5a1a-e53be2de7118>
2011-05-27 09:39:17 +0000

    commited missing file

---
+++ Othello/src/MaClasseMatricePlateau.java
@@ -0,0 +1,64 @@
+
+import java.util.Random;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andre
+ */
+public class MaClasseMatricePlateau {
+
+    private Couleur[][] matricePlateau;
+    private float valHeuristique;
+
+    public MaClasseMatricePlateau()
+    {
+    }
+
+    public MaClasseMatricePlateau(Couleur[][] matricePlateau)
+    {
+        this.matricePlateau = matricePlateau;
+    }
+
+
+
+
+    public Couleur[][] getMatricePlateau()
+    {
+        return matricePlateau;
+    }
+
+    public void setIdent(Couleur[][] matricePlateau)
+    {
+        this.matricePlateau = matricePlateau;
+    }
+
+    public float getValHeuristique()
+    {
+        return valHeuristique;
+    }
+
+    public void setValHeuristique(float valHeuristique)
+    {
+        this.valHeuristique = valHeuristique;
+    }
+
+    public void setValRandomHeuristique()
+    {
+        Random r = new Random();
+        setValHeuristique(r.nextFloat());
+    }
+
+    /*
+    @Override
+    public String toString()
+    {
+        return Integer.toString(plateau);
+    }
+     *
+     */
+}
