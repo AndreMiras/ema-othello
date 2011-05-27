b1f74bd1e13867d6a1a3101f863a891ea57ebec9
nico.tomio@gmail.com <nico.tomio@gmail.com@f8d6e94b-3401-3f3e-5a1a-e53be2de7118>
2011-05-27 09:29:01 +0000

    fonction pour retourner les pions

--- Othello/src/AbstractJoueur.java
+++ Othello/src/AbstractJoueur.java
@@ -101,8 +101,7 @@ abstract class AbstractJoueur implements InterfaceJoueur
                }
             }
             ligne++;
-        }
- 
+        } 
 
         return tabCaseVide;
     }
@@ -164,6 +163,136 @@ abstract class AbstractJoueur implements InterfaceJoueur
              
         return coupPossible;
     }
+    
+    //Fonction pour recopier un tableau
+    public Couleur[][] copieTableau (Plateau plateau)
+    {
+        Couleur newPlateau[][] = new Couleur[plateau.getDimension()][plateau.getDimension()];
+        
+        for (int i=0; i<plateau.getDimension(); i++)
+        {
+            System.arraycopy(plateau.getMatricePlateau()[i], 0, newPlateau[i], 0, plateau.getDimension());
+        }    
+        
+        return newPlateau;
+    }
+    
+    //Fonction pour retourner les pions
+    public Couleur[][] retournerPions (Couleur[][] matricePlateau, Couleur couleur, Coup coup)
+    {
+        int x = 0; //Variable qui determine la direction horizontale
+        int y = 0; //Variable qui determine la direction verticale
+        //Variables pour analyser les cases
+        int pointLigne;
+        int pointColonne;
+  
+        //Condition pour sortir de boucle
+        boolean flag = true;
+
+        
+        ArrayList<Coup> tabCaseAdverseAutour = chercheCouleurAdverseAutour(coup, matricePlateau); 
+        for (int j=0; j<tabCaseAdverseAutour.size();j++)
+        {
+            // On cherche la direction en x et y
+            x=-(coup.getColonne()-tabCaseAdverseAutour.get(j).getColonne());
+            y=-(coup.getLigne()-tabCaseAdverseAutour.get(j).getLigne());
+
+            //On fixe la case à traiter
+            pointColonne = coup.getColonne()+x;
+            pointLigne = coup.getLigne()+y;
+            //On boucle tant qu'on est dans le tableau
+            flag = true;
+            while(pointColonne>0 && pointColonne<matricePlateau.length && pointLigne>0 && pointLigne<matricePlateau.length && flag==true)
+            {
+                //On cherche une case de la couleur du joueur, si on en trouve
+                //une, on fait marche arriere pour retourner les pions
+                if(matricePlateau[pointLigne][pointColonne] == this.couleur)
+                {
+                    pointColonne = pointColonne -x;
+                    pointLigne = pointLigne - y;
+                    while(pointColonne != coup.getColonne() && pointLigne != coup.getLigne())
+                    {
+                        matricePlateau[pointLigne][pointColonne] = this.couleur;
+                        pointColonne = pointColonne -x;
+                        pointLigne = pointLigne - y;
+                    }
+                    flag = false;
+                    
+                }
+                //Si on tombe sur une case vide, on sort de la boucle
+                else if(matricePlateau[pointLigne][pointColonne] == Couleur.VIDE)
+                {
+                    flag=false;
+                }
+                //On déplace la case à traiter
+                pointColonne=pointColonne+x;
+                pointLigne=pointLigne+y;
+            }
+        }  
+             
+        return matricePlateau;
+    }
+    
+        //Fonction qui retourne un tableau des couleur adverse autour d'un pion
+    private ArrayList<Coup> chercheCouleurAdverseAutour(Coup coup, Couleur[][] matricePlateau)
+    {
+        ArrayList<Coup> tabCaseCouleurAdverse = new ArrayList<Coup>();
+        
+        int lignePrecedente = coup.getLigne()-1;
+        int ligneSuivante = coup.getLigne()+1;
+        int colonnePrecedente = coup.getColonne()-1;
+        int colonneSuivante = coup.getColonne()+1;
+
+        int ligne;
+        int colonne;
+        boolean lignePossible = true;
+        boolean colonnePossible = true;
+                
+        ligne = lignePrecedente;
+        while (ligne<=ligneSuivante && lignePossible)
+        {
+            if(ligne<0)
+            {
+                lignePossible=false;
+            }
+            else if(ligne >=plateau.getDimension())
+            {
+                lignePossible=false;
+            }
+            else
+            {
+               colonnePossible=true;
+               colonne = colonnePrecedente;
+               while (colonne<=colonneSuivante && colonnePossible)
+               {
+                   
+                   if(colonne<0)
+                   {
+                       colonnePossible=false;
+                   }
+                   else if(colonne >= plateau.getDimension())
+                   {
+                       colonnePossible=false;
+                   }
+                   else
+                   {
+                        if(matricePlateau[ligne][colonne] != this.couleur && matricePlateau[ligne][colonne] != Couleur.VIDE)
+                        {
+                            tabCaseCouleurAdverse.add(new Coup(ligne, colonne));
+                        }
+                   }
+                   colonne++;
+               }
+            }
+            ligne++;
+        } 
+
+        return tabCaseCouleurAdverse;
+    }
+    
+    
+    
+    
 
     
     public TypeJoueur getType()
@@ -180,5 +309,6 @@ abstract class AbstractJoueur implements InterfaceJoueur
     {
         return plateau;
     }
-    
+
+
 }
