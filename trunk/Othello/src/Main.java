05ee0ff3a3e8a12e7622d26d317db66e6061454b
andre.miras <andre.miras@f8d6e94b-3401-3f3e-5a1a-e53be2de7118>
2011-05-28 11:34:37 +0000

    removed all this (couleur/plateau) ref in the abstract joueur so we can simulate a game passing anything

--- trunk/Othello/src/Main.java
+++ trunk/Othello/src/Main.java
@@ -14,9 +14,9 @@ public class Main {
         // création d'un plateau
         Plateau plateau = new Plateau();
         //déclaration des joueurs
-        AbstractJoueur joueur1 = new JoueurRandomIA(Couleur.NOIR, plateau,TypeJoueur.AUTOMATE);
+        AbstractJoueur joueur1 = new JoueurRandomIA(Couleur.NOIR, plateau,TypeJoueur.HUMAIN);
         // Joueur joueur2 = new Joueur(Couleur.BLANC, plateau, TypeJoueur.AUTOMATE);
-        AbstractJoueur joueur2 = new JoueurMeilleurCoup1(Couleur.BLANC, plateau, TypeJoueur.AUTOMATE);
+        AbstractJoueur joueur2 = new JoueurMeilleurCoup1(Couleur.BLANC, plateau, TypeJoueur.HUMAIN);
         //création de l'IHM du plateau
         IHMplateau ihm = new IHMplateau(plateau, joueur1, joueur2);
         //association de l'IHM au plateau
