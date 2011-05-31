76544498bc1d4cf154639e724db0d166cb42302e
andre.miras <andre.miras@f8d6e94b-3401-3f3e-5a1a-e53be2de7118>
2011-05-31 16:50:08 +0000

    corner and hedge heuristic

--- trunk/Othello/src/InfoMatricePlateau.java
+++ trunk/Othello/src/InfoMatricePlateau.java
@@ -8,6 +8,21 @@
  * valHeuristique = TODO: metttre à jour avec la véritable heuristique
  */
 public class InfoMatricePlateau implements Comparable<InfoMatricePlateau>  {
+
+    private int cornerHeuristicValue = 9;
+    private int hedgeHeuristicValue = 3;
+    private int ponderationMatrice[][] =
+    {
+        {cornerHeuristicValue, 1, 1, 1, 1, 1, 1,  cornerHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {hedgeHeuristicValue, 1, 1, 1, 1, 1, 1,  hedgeHeuristicValue},
+        {cornerHeuristicValue, 1, 1, 1, 1, 1, 1,  cornerHeuristicValue}
+    };
+
     
     protected Couleur[][] matricePlateau;
     /*
@@ -70,7 +85,7 @@ public class InfoMatricePlateau implements Comparable<InfoMatricePlateau>  {
      * calcule la valeur heuristique a partir d'une matrice de couleur
      * @return: calcul et retourne la valeur heuristique a partir d'une matrice de couleur
      */
-    private Integer computeHeuristicValue()
+    private Integer totalPionHeuristicValue()
     {
         int totalPieces = 0;
         int opponentTotalPieces = 0;
@@ -92,6 +107,33 @@ public class InfoMatricePlateau implements Comparable<InfoMatricePlateau>  {
         }
 
         return totalPieces - opponentTotalPieces;
+    }
+
+    /**
+     * pondère les cotés et les coins
+     * @return: le coefficient de ponderation de l'heuristique générale
+     */
+    private int pondereHeuristicValue()
+    {
+        /*
+        int poids = 1;
+        if (coup.getLigne() == 0 || coup.getLigne() == 7)
+        {
+            poids *= borderHeuristicValue;
+        }
+        if (coup.getColonne() == 0 || coup.getColonne() == 7)
+        {
+            poids *= borderHeuristicValue;
+        }
+         * 
+         */
+        
+        return ponderationMatrice[coup.getLigne()][coup.getColonne()];
+    }
+
+    private int computeHeuristicValue()
+    {
+        return totalPionHeuristicValue() * pondereHeuristicValue();
     }
 
     public int compareTo(InfoMatricePlateau t)
